fecd30b0a89299456e498da50643e65ab94d5c54
Juan Vidal <juanmvg2003@gmail.com>
2023-07-03 12:30:26 -0500

    Consume FakeApi Platzi API

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,15 +1,15 @@
 
-const Card = () => {
+const Card = ({category, productImg, productName, productPrice }) => {
     return (
         <div className="border transition-all border-gray-500 bg-whitem rounded-t-lg cursor-pointer w-56 h-72 overflow-hidden hover:-translate-y-2 hover:shadow-lg">
-            <figure className="relative mb-4 w-full h-4/5">
-                <span className="absolute bottom-2 left-2 bg-zinc-800 px-3 text-white text-sm py-1 rounded-full z-50">Electronics</span>
-                <img src="https://images.pexels.com/photos/7857202/pexels-photo-7857202.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="absolute w-full h-full object-cover"/>
+            <figure className="relative mb-4 w-full h-4/5 overflow-hidden">
+                <span className="absolute bottom-2 left-2 bg-zinc-800 px-3 text-white text-sm py-1 rounded-full z-50">{category}</span>
+                <img src={productImg} alt="" className="absolute w-full h-full object-cover transition hover:scale-110"/>
                 <div className="absolute top-3 right-3 flex justify-center items-center bg-white w-6 h-6 rounded-full">+</div>
             </figure>
             <p className="px-3 flex justify-between items-center">
-                <span className="text-sm">HeadPhones</span>
-                <span className="font-bold text-lg">$300</span>
+                <span className="text-sm">{productName}</span>
+                <span className="font-bold text-lg">${productPrice}</span>
             </p>
         </div>
     );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,5 @@
+// react hooks
+import { useState, useEffect } from "react";
 // base strcuture
 import Layout from "../../components/layout";
 // card component
@@ -5,9 +7,26 @@ import Card from "../../components/Card";
 
 // home component
 const Home = () => {
+    
+    const [items, setItems] = useState(null);
+
+    // getting all items
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+            .then(res => res.json())
+            .then(data => setItems(data))
+    }, []);
+
     return (
         <Layout>
-            <Card />
+            Home
+            <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg mt-16">
+            {
+                items?.map(item => {
+                    return <Card key={item.id} category={item.category.name} productImg={item.images[0]} productName={item.title} productPrice={item.price} />
+                })
+            }
+            </div>
         </Layout>
     );
 }

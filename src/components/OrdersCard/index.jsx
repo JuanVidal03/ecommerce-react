7fab1367edcaff4a7a42cb6e283b9337cb08a96c
Juan Vidal <juanmvg2003@gmail.com>
2023-07-27 15:57:44 -0500

    It was created and finished checkout

--- src/components/OrdersCard/index.jsx
+++ src/components/OrdersCard/index.jsx
@@ -1,14 +1,20 @@
 // heroicons
-import { XMarkIcon } from '@heroicons/react/24/solid';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 
 const OrdersCard = ({ totalPrice, totalProducts }) => {
 
-
     return (
-        <div className='flex items-center border border-black rounded-lg gap-2'>
-            <h5>Total Price: { totalPrice }</h5>
-            <h6>Total Products: { totalProducts }</h6>
-            <p>Date: 23/07/2023</p>
+        <div className='border border-black rounded-lg gap-2  p-3 my-3 w-80'>
+            <div className='w-full flex justify-between'>
+                <div className='flex flex-col w-50'>
+                    <p>23/07/2023</p>
+                    <h6>{ totalProducts } articles</h6>
+                </div>
+                <div className='flex items-center gap-2 w-50'>
+                    <h5 className='font-bold'>${ totalPrice } USD</h5>
+                    <MagnifyingGlassIcon className="h-6 w-6 text-black font-bold transition-all hover:text-zinc-800" />
+                </div>
+            </div>
         </div>
     );
 }
